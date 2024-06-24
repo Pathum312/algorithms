db62e847c8cdf092e8c05a729957a000313844cd
Pathum312 <pathum.2019557@iit.ac.lk>
2024-06-24 23:47:09 +0530

    Project Initialization

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,105 @@
+<div class="container">
+	<header>Algorithms</header>
+	<main>
+		<section>
+			<h2>Sorting Algorithms</h2>
+			<div class="card">
+				<h3>Bubble Sort</h3>
+				<p class="description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere blanditiis, asperiores
+					deserunt similique unde distinctio ipsam maxime rem, veniam placeat esse obcaecati, culpa
+					quasi repudiandae.
+				</p>
+			</div>
+			<div class="card">
+				<h3>Other Sort</h3>
+				<p class="description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere blanditiis, asperiores
+					deserunt similique unde distinctio ipsam maxime rem, veniam placeat esse obcaecati, culpa
+					quasi repudiandae.
+				</p>
+			</div>
+			<div class="card">
+				<h3>Tharaka Sort</h3>
+				<p class="description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere blanditiis, asperiores
+					deserunt similique unde distinctio ipsam maxime rem, veniam placeat esse obcaecati, culpa
+					quasi repudiandae.
+				</p>
+			</div>
+		</section>
+		<section>
+			<h2>Searching Algorithms</h2>
+			<div class="card">
+				<h3>Bubble Sort</h3>
+				<p class="description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere blanditiis, asperiores
+					deserunt similique unde distinctio ipsam maxime rem, veniam placeat esse obcaecati, culpa
+					quasi repudiandae.
+				</p>
+			</div>
+			<div class="card">
+				<h3>Other Sort</h3>
+				<p class="description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere blanditiis, asperiores
+					deserunt similique unde distinctio ipsam maxime rem, veniam placeat esse obcaecati, culpa
+					quasi repudiandae.
+				</p>
+			</div>
+			<div class="card">
+				<h3>Tharaka Sort</h3>
+				<p class="description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere blanditiis, asperiores
+					deserunt similique unde distinctio ipsam maxime rem, veniam placeat esse obcaecati, culpa
+					quasi repudiandae.
+				</p>
+			</div>
+		</section>
+	</main>
+	<footer>© 2024 Pathum Senanayake</footer>
+</div>
+
+<style>
+	.container {
+		/* padding: 1rem; */
+		background-color: antiquewhite;
+	}
+
+	header {
+		padding: 1rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--clr-white-50);
+		font-size: var(--fs-heading-0);
+		background-color: var(--clr-prussian-blue);
+	}
+
+	footer {
+		padding: 1rem;
+		display: flex;
+		align-items: center;
+		font-size: var(--fs-0);
+		justify-content: center;
+		color: var(--clr-white-50);
+		background-color: var(--clr-prussian-blue);
+	}
+
+	main {
+		padding: 1rem;
+		background-color: var(--clr-white-50);
+	}
+
+	.card {
+		padding: 1rem;
+		margin: 1rem 0;
+		border-radius: 0.5rem;
+		filter: var(--shadow-3);
+		color: var(--clr-white-1);
+		background-color: var(--clr-white);
+	}
+
+	.description {
+		max-width: 70ch;
+	}
+</style>
